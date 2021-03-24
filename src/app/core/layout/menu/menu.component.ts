631eaea46aa7f19f38a99244b95c19c44c194c10
Camilo Hernandez <juanc.h@tcs.com>
2021-03-24 15:56:29 -0500

    Ajuste menu

--- src/app/core/layout/menu/menu.component.ts
+++ src/app/core/layout/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WorkFlowService } from 'src/app/core/services/workflow/work-flow.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  subscription = new Subscription;
+  dataMenu: any = {};
+
+  constructor(
+    private workflow: WorkFlowService
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.workflow.getPayload().subscribe((resp) => {
+      if (resp.dataMenu) {
+        this.dataMenu = resp.dataMenu;
+        const fullName = this.dataMenu.name.split(" ");
+        this.dataMenu.firstName = fullName[0];
+        this.dataMenu.secondName = fullName[1];
+        this.dataMenu.firstLastName = fullName[2];
+        this.dataMenu.secondLastName = fullName[3];
+      }
+    });
   }
 
 }
