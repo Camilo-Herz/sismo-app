8f4da958b9b67201b85955313e593872c99fa1c0
Camilo Hernandez <juancas.1420@gmail.com>
2021-03-31 13:22:00 -0500

    correccion server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(`${__dirname}/dist/sismo`));
 app.get('*', (req, res) => {
-    res.sendFile('./dist/sismo/index.html');
+    res.sendFile(path.join(__dirname +
+        '/dist/sismo/index.html'));
 });
 app.listen(process.env.PORT || 8080);
