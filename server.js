4cb2ce93eecb99171713c8b38807d21a8b794d5c
Camilo Hernandez <juancas.1420@gmail.com>
2021-04-02 13:52:08 -0500

    Ajuste

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/sismo'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/sismo/index.html'));
+app.use(express.static(__dirname + '/dist/sismo'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/sismo/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
