be3d8c656186ad24fb32c4a007445b65c3e82a9c
Camilo Hernandez <juancas.1420@gmail.com>
2021-04-07 21:20:26 -0500

    Procesos

--- src/app/modules/processes/processes.component.ts
+++ src/app/modules/processes/processes.component.ts
@@ -7,7 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessesComponent implements OnInit {
 
-  constructor() { }
+  single: any = [];
+  multi: any = [];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() { 
+    this.single = [
+      {
+        "name": "Germany",
+        "value": 8940000
+      },
+      {
+        "name": "USA",
+        "value": 5000000
+      },
+      {
+        "name": "France",
+        "value": 7200000
+      }
+    ]
+  }
+
+  onSelect(event: any) {
+    console.log(event);
+  }
 
   ngOnInit(): void {
   }
