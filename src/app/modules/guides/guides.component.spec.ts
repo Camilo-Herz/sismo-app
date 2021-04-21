b3e7642fc766bbfc6ee285c546418a45aff4199b
Camilo Hernandez <juancas.1420@gmail.com>
2021-04-21 01:29:41 -0500

    Ajuste ultimos cambios

--- src/app/modules/guides/guides.component.spec.ts
+++ src/app/modules/guides/guides.component.spec.ts
@@ -19,7 +19,7 @@ describe('ManualsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
