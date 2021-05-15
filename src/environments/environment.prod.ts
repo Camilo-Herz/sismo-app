0593a600be558690505a6e2116050c5a88ab8073
Camilo Hernandez <juancas.1420@gmail.com>
2021-05-15 08:37:06 -0500

    Ajuste despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   workflowUrl: 'https://sismo-service.herokuapp.com',
+  boxPlotCalculation: 'http://www.alcula.com/es/calculadoras/estadistica/php_pub/submit_data.php',
   title: 'SISMO'
 };
