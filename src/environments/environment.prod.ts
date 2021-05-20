3aebc65cf69b3ac9e3c65a2fe2e2c532e96ba7b3
Camilo Hernandez <juancas.1420@gmail.com>
2021-05-20 11:45:10 -0500

    Ajuste diagrama de cajas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   workflowUrl: 'https://sismo-service.herokuapp.com',
   boxPlotCalculation: 'http://www.alcula.com/es/calculadoras/estadistica/php_pub/submit_data.php',
+  outliers: 'https://miniwebtool.com/es/outlier-calculator/',
   title: 'SISMO'
 };
